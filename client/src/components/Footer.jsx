0597a2f572a03b03413c1405149c32d77698afcc
root <root@ferdinandimeka>
2023-11-28 22:45:44 +0100

    footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <footer>
+        <div className='flex flex-col items-center justify-center bg-violet-900 py-10'>
+            <p className='text-sm text-gray-100'>
+                <p className="text-2xl font-bold ml-3 mr-auto">
+                <span className="text-red-500">M</span>
+                <span className="text-blue-500">-</span>
+                <span className="text-yellow-500">B</span>
+                <span className="text-indigo-500">o</span>
+                <span className="text-green-500">o</span>
+                <span className="text-pink-500">k</span>
+                <span className="text-purple-500">.com</span>
+            </p>
+                &copy; 2023 All rights reserved.
+            </p>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
